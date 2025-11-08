63949a6d32d0f962b0c94656f04d95a6b70b8e1f
luqfianita8317 <luqfianita8317@gmail.com>
2025-11-08 21:31:09 +0700

    Refactor and enhance loan prediction app

--- pinjaman.py
+++ pinjaman.py
@@ -1,5 +1,3 @@
-Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -211,3 +209,4 @@ with tab2:
 ...     ax2.set_title('Feature Importances Heatmap (Decision Tree)')
 ...     ax2.set_yticks([]) 
 ...     st.pyplot(fig2)
+
